f99b5a1209d5ef44ee70dfdda39a26451bc8ae7c
tupatil <see2tushar@gmail.com>
2023-05-30 16:28:24 +0530

    v

--- abc.ts
+++ abc.ts
@@ -1,92 +1,73 @@
-{
-  headerName: 'Name',
-  field: 'name',
-  editable: true,
-  cellEditor: 'popupEditor',
-  cellEditorParams: {
-    component: 'popupEditorComponent'
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-edit-dialog',
+  template: `
+    <h2>Edit Value</h2>
+    <input [(ngModel)]="data.value" />
+    <button mat-raised-button color="primary" (click)="save()">Save</button>
+    <button mat-raised-button (click)="close()">Cancel</button>
+  `,
+})
+export class EditDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<EditDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  save(): void {
+    this.dialogRef.close(this.data.value);
+  }
+
+  close(): void {
+    this.dialogRef.close();
   }
 }
 
+
+MatDialogModule
+
 import { Component } from '@angular/core';
-import { ICellEditorAngularComp } from 'ag-grid-angular';
+import { ICellRendererAngularComp } from 'ag-grid-angular';
+import { MatDialog } from '@angular/material/dialog';
+import { EditDialogComponent } from './edit-dialog.component';
 
 @Component({
-  selector: 'app-popup-editor',
+  selector: 'app-edit-cell-renderer',
   template: `
-    <div class="popup-editor">
-      <input [(ngModel)]="value" (keydown.enter)="onEnter()" (keydown.esc)="onCancel()" (focusout)="onEnter()">
-    </div>
+    <div (click)="openDialog()">{{ value }}</div>
   `,
-  styles: [
-    `
-    .popup-editor {
-      position: absolute;
-      background-color: white;
-      border: 1px solid lightgrey;
-      padding: 5px;
-      z-index: 1000;
-    }
-    `
-  ]
 })
-export class PopupEditorComponent implements ICellEditorAngularComp {
-  private params: any;
+export class EditCellRendererComponent implements ICellRendererAngularComp {
   public value: any;
+  private params: any;
+
+  constructor(private dialog: MatDialog) {}
 
   agInit(params: any): void {
     this.params = params;
-    this.value = this.params.value;
+    this.value = params.value;
   }
 
-  getValue(): any {
-    return this.value;
-  }
-
-  isPopup(): boolean {
+  refresh(params: any): boolean {
+    this.params = params;
+    this.value = params.value;
     return true;
   }
 
-  onEnter(): void {
-    this.params.stopEditing();
-  }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(EditDialogComponent, {
+      width: '250px',
+      data: { value: this.value },
+    });
 
-  onCancel(): void {
-    this.params.stopEditing(true);
-  }
-}
-
-import { NgModule } from '@angular/core';
-import { AgGridModule } from 'ag-grid-angular';
-import { PopupEditorComponent } from './popup-editor.component';
-
-@NgModule({
-  declarations: [PopupEditorComponent],
-  imports: [AgGridModule.withComponents([PopupEditorComponent])]
-})
-export class AppModule { }
-
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-grid',
-  template: `
-    <ag-grid-angular [rowData]="rowData" [columnDefs]="columnDefs" [frameworkComponents]="frameworkComponents">
-    </ag-grid-angular>
-  `
-})
-export class GridComponent {
-  rowData: any[];
-  columnDefs: any[];
-  frameworkComponents: any;
-
-  constructor() {
-    this.rowData = /* your row data */;
-    this.columnDefs = [
-      // Column definitions including the one with the custom editor
-    ];
-    this.frameworkComponents = {
-      popupEditorComponent: PopupEditorComponent
-    };
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result !== undefined) {
+        this.value = result;
+        this.params.data[this.params.colDef.field] = result;
+        this.params.api.refreshCells();
+      }
+    });
   }
 }
