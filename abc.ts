617c2a764552ea87ff215364207a03c6721ba96f
tupatil <see2tushar@gmail.com>
2023-06-25 13:58:02 +0530

    12

--- abc.ts
+++ abc.ts
@@ -1,18 +1,27 @@
-<mat-slider thumbLabel tickInterval="1" [(ngModel)]="value" min="0" max="2" class="custom-slider"></mat-slider>
-<div class="labels">
-  <span>Low</span>
-  <span>Medium</span>
-  <span>High</span>
-</div>
+import { Component } from '@angular/core';
 
-.custom-slider .mat-slider-track-wrapper {
-  background-image: linear-gradient(to right, red, gray, green);
+@Component({
+  selector: 'app-slider',
+  template: `
+    <mat-slider thumbLabel tickInterval="1" [(ngModel)]="value" min="0" max="2" class="custom-slider"></mat-slider>
+    <div class="labels">
+      <span>Low</span>
+      <span>Medium</span>
+      <span>High</span>
+    </div>
+  `,
+  styles: [`
+    .custom-slider .mat-slider-track-wrapper {
+      background-image: linear-gradient(to right, red, gray, green);
+    }
+    
+    .labels {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 16px;
+    }
+  `]
+})
+export class SliderComponent {
+  value: number = 1;
 }
-
-.labels {
-  display: flex;
-  justify-content: space-between;
-  margin-top: 16px;
-}
-
-value: number = 1;
