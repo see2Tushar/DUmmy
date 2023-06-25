a2ea22486d882cad8040f77eb64f2b72d40fd0be
tupatil <see2tushar@gmail.com>
2023-06-25 13:55:53 +0530

    123

--- abc.ts
+++ abc.ts
@@ -1,55 +1,18 @@
-import { Component } from '@angular/core';
-import { AgEditorComponent } from 'ag-grid-angular';
+<mat-slider thumbLabel tickInterval="1" [(ngModel)]="value" min="0" max="2" class="custom-slider"></mat-slider>
+<div class="labels">
+  <span>Low</span>
+  <span>Medium</span>
+  <span>High</span>
+</div>
 
-@Component({
-  selector: 'app-custom-select-cell-editor',
-  template: `
-    <select [disabled]="!shouldOpenEditor" (keydown)="onKeyDown($event)">
-      <option *ngFor="let option of values" [value]="option">{{ option }}</option>
-    </select>
-  `
-})
-export class CustomSelectCellEditor implements AgEditorComponent {
-  shouldOpenEditor: boolean;
-  values: any[];
-
-  agInit(params: any): void {
-    this.shouldOpenEditor = params.shouldOpenEditor;
-    this.values = params.values;
-  }
-
-  getValue(): any {
-    // Return the selected value or any other logic as needed
-    // For example, you can return the selected value from the select element
-    return 'TODO: Implement getValue()';
-  }
-
-  onKeyDown(event: KeyboardEvent): void {
-    // Handle key events if required
-  }
+.custom-slider .mat-slider-track-wrapper {
+  background-image: linear-gradient(to right, red, gray, green);
 }
 
+.labels {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 16px;
+}
 
-const columnDefs = [
-  {
-    headerName: 'Country',
-    field: 'country',
-    cellEditor: 'customSelectCellEditor',
-    cellEditorParams: {
-      values: ['USA', 'Canada', 'UK', 'Australia'],
-      shouldOpenEditor: true // Provide the condition flag to the cell editor
-    }
-  },
-  // Other column definitions...
-];
-
-
-import { NgModule } from '@angular/core';
-import { AgGridModule } from 'ag-grid-angular';
-import { CustomSelectCellEditor } from './custom-select-cell-editor.component';
-
-@NgModule({
-  declarations: [CustomSelectCellEditor],
-  imports: [AgGridModule.withComponents([CustomSelectCellEditor])]
-})
-export class AppModule { }
+value: number = 1;
