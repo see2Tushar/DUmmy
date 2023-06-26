30344b6766daeba12b0e898bba5ef3c4e7a4f64a
tupatil <see2tushar@gmail.com>
2023-06-26 10:34:02 +0530

    bnn

--- abc.ts
+++ abc.ts
@@ -1,48 +1,85 @@
-.toggle-container {
-  display: inline-block;
-  position: relative;
-  width: 80px;
-  height: 34px;
-}
+import { Component, Input } from '@angular/core';
 
-.toggle-option {
-  display: flex;
-  align-items: center;
-  height: 100%;
-  margin: 0;
-  position: relative;
-  cursor: pointer;
-}
+@Component({
+  selector: 'app-three-way-toggle',
+  template: `
+    <div class="toggle-container">
+      <label class="toggle-option">
+        <input
+          type="radio"
+          [value]="'option1'"
+          [checked]="toggleValue === 'option1'"
+          (change)="onToggleChange($event.target.value)"
+        />
+        <span class="toggle-slider"></span>
+      </label>
+      <label class="toggle-option">
+        <input
+          type="radio"
+          [value]="'option2'"
+          [checked]="toggleValue === 'option2'"
+          (change)="onToggleChange($event.target.value)"
+        />
+        <span class="toggle-slider"></span>
+      </label>
+      <label class="toggle-option">
+        <input
+          type="radio"
+          [value]="'option3'"
+          [checked]="toggleValue === 'option3'"
+          (change)="onToggleChange($event.target.value)"
+        />
+        <span class="toggle-slider"></span>
+      </label>
+    </div>
+  `,
+  styles: [`
+    .toggle-container {
+      display: flex;
+      align-items: center;
+      height: 34px;
+      width: 180px;
+      background-color: #ccc;
+      border-radius: 17px;
+      overflow: hidden;
+    }
 
-.toggle-slider {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #ccc;
-  transition: background-color 0.2s;
-}
+    .toggle-option {
+      flex: 1;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      transition: background-color 0.2s;
+    }
 
-.toggle-option input {
-  display: none;
-}
+    .toggle-option:hover {
+      background-color: #ddd;
+    }
 
-.toggle-option input:checked + .toggle-slider {
-  background-color: #2196f3;
-}
+    .toggle-option input {
+      display: none;
+    }
 
-.toggle-option input:checked + .toggle-slider::before {
-  transform: translateX(46px);
-}
+    .toggle-slider {
+      width: 50px;
+      height: 30px;
+      background-color: #fff;
+      border-radius: 15px;
+      transition: transform 0.2s;
+    }
 
-.toggle-slider::before {
-  content: '';
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  bottom: 2px;
-  width: 28px;
-  background-color: #fff;
-  transition: transform 0.2s;
+    .toggle-option input:checked + .toggle-slider {
+      transform: translateX(90px);
+    }
+  `]
+})
+export class ThreeWayToggleComponent {
+  @Input() toggleId: string;
+  toggleValue: string = 'option1';
+
+  onToggleChange(value: string) {
+    this.toggleValue = value;
+  }
 }
